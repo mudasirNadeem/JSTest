714f45fe37c28ac8a710ec8be58f40f1e0590b79
mudasir Nadeem <mudasirnadeem7979@gmail.com>
2025-01-08 15:23:46 +0500

    commit

--- vertical-slider/vertical-slider.js
+++ vertical-slider/vertical-slider.js
@@ -1,17 +1,42 @@
-
 var firstElement = document.getElementById('first-element');
-// var secondElement = document.getElementById('twoElement');
-// var thredElement = document.getElementById('threeElement');
-// var thredElement = document.getElementById('fourElement');
-var sumOfValue = 0 ;
+var secondElement = document.getElementById('twoElement');
+var thirdElement = document.getElementById('threeElement');
+var fourthElement = document.getElementById('fourthElement');
+var sumOfValue = 0;
+function updateSliderPosition() {
+    firstElement.style.marginTop = sumOfValue >= 1 ? '-100vh' : '0vh';
+    firstElement.style.transition = sumOfValue >= 1 ? '1s' : '1s';
+    secondElement.style.marginTop = sumOfValue >= 2 ? '-100vh' : '0vh';
+    secondElement.style.transition = sumOfValue >= 1 ? '1s' : '1s';
+    thirdElement.style.marginTop = sumOfValue >= 3 ? '-100vh' : '0vh';
+    thirdElement.style.transition = sumOfValue >= 1 ? '1s' : '1s';
+}
+
 function arrowUp() {
-    // console.log('sumOfValue:', sumOfValue);
-    if (sumOfValue == 0) {
-        firstElement.style.marginTop = '-100vh';
-        console.log('Moved #first-element up');
+    if (sumOfValue < 3) {
         sumOfValue++;
     } else {
-        firstElement.style.marginTop = '0px';
-        console.log('Reset #first-element');
+        sumOfValue = 0;
+        firstElement.style.marginTop = '0vh';
+        secondElement.style.marginTop = '0vh';
+        thirdElement.style.marginTop = '0vh';
     }
+    updateSliderPosition();
+}
+
+function arrowDown() {
+    if (sumOfValue > 0) {
+        sumOfValue--;
+    } else {
+        sumOfValue = 3;
+        firstElement.style.marginTop = '-300vh';
+        firstElement.style.transition = '1s';
+        secondElement.style.marginTop = '0vh';
+        secondElement.style.transition = '1s';
+        thirdElement.style.marginTop = '0vh';
+        thirdElement.style.transition = '1s';
+
+
+    }
+    updateSliderPosition();
 }
